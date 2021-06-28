51ab868dff807908932c37aff416e2da513be10b
zpskek <wjdghdwns0@gmail.com>
2021-06-28 16:21:33 +0900

    Setup dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+    ignoreEnvFile: process.env.NODE_ENV === 'production',
+    validationSchema: Joi.object({
+      NODE_ENV: Joi.string().valid('dev', 'production', 'test').required()
+    })
+  })],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config.ts
@@ -0,0 +1,21 @@
+const config = {
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+  },
+
+  private_key: process.env.PRIVATE_KEY,
+  mail: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain_name: process.env.MAILGUN_DOMAIN_NAME,
+    from_email: process.env.MAILGUN_FROM_EMAIL,
+  },
+  aws: {
+    access_key: process.env.AWS_ACCESS_KEY,
+    secret_key: process.env.AWS_SECRET_KEY,
+  }
+
+}
