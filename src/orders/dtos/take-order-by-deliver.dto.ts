75811f2e0c4db4e5f1dfad52545507a2a92dbfc8
zpskek <wjdghdwns0@gmail.com>
2021-08-19 12:37:07 +0900

    Remove unused module on take-order-by-deliver dto

--- src/orders/dtos/take-order-by-deliver.dto.ts
+++ src/orders/dtos/take-order-by-deliver.dto.ts
@@ -1,6 +1,5 @@
 import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
 import { CoreOutput } from 'src/common/dtos/output.dto';
-import { Order, OrderStatus } from '../entities/order.entity';
 
 @InputType()
 export class TakeOrderByDeliverInput {
